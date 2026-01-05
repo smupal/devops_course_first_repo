a8eacdf3c682f0cfbe914b2c6df8a6da83181ab4
sumit <smupal@gmail.com>
2026-01-06 01:02:22 +0530

    up

---
+++ Ansible/Terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket=abc3376"
+
+  tags = {
+    Name        = "My bucket terraform"
+    Environment = "Dev"
+  }
+}
