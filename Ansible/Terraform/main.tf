f58949c1aeec0e16135be6ea76013158db14d2d6
sumit <smupal@gmail.com>
2026-01-06 01:25:25 +0530

    up

--- Ansible/Terraform/main.tf
+++ Ansible/Terraform/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket=ajindina"
+  bucket = "my-tf-test-bucket-abc3376"
 
   tags = {
     Name        = "My bucket terraform"
